b16c86fbf248339368158d05959c679c621e37e8
IrenePletneva <73946301+IrenePletneva@users.noreply.github.com>
2023-09-25 19:19:23 +0300

    add browser version

--- src/test/java/guru/qa/tests/RemoteTestBase.java
+++ src/test/java/guru/qa/tests/RemoteTestBase.java
@@ -18,6 +18,7 @@ public class RemoteTestBase {
 
         Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.pageLoadStrategy = "eager";
 
